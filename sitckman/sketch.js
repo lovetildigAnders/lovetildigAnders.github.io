3a172e559092ace5921b0e47281a0720565c2978
lovetildigAnders <asylvesterp@gmail.com>
2020-10-08 09:26:32 +0200

    stickman

---
+++ sitckman/sketch.js
@@ -0,0 +1,21 @@
+let gg= 30
+function setup() {
+  createCanvas(400, 400);
+   
+}
+function draw() {
+  background(200);
+}
+function draw() {
+
+  gg= gg 
+  line(200, 80, 200, 210)
+  line(200, 100, 180, 190)
+  line(200, 100, 220, 190)
+  line(200, 210, 180, 290)
+  line(200, 210, 220, 290)
+  circle(200, 80, 30)
+  line(180, 190, 190, 200) 
+  line(180, 190, 170, 180)
+  triangle(160,190,175,185,120,120)
+}
