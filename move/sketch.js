0dd140b02cfa825b810ba826bf03c8aa673b90ef
lovetildigAnders <asylvesterp@gmail.com>
2020-10-22 10:37:54 +0200

    maybe

--- move/sketch.js
+++ move/sketch.js
@@ -1,11 +1,18 @@
 let x= 30
 let y= 30
+let goleft = false; 
 function setup() {
   createCanvas(400, 400);
 }
 function draw() {
   background(200,200,0);
-  circle(x, y, 30)
-  x=x+1
-  y=y+1
+  circle(x, y, 30);
+if(goleft ==false){
+  x=x-1
+}
+if(x=200) 
+{
+  goleft =true;
+}
+ 
 }
