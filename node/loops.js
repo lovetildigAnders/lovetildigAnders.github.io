6375e0cb4dc63972e69aaab0c6e79163705bae98
lovetildigAnders <asylvesterp@gmail.com>
2020-11-05 10:26:08 +0100

    loops

---
+++ node/loops.js
@@ -0,0 +1,14 @@
+
+//opg 1 tæl ned fra 10 - med while løkke
+console.log("opg 1")
+let i=10
+while (i>=1){
+    console.log(i);
+    i--;
+}
+
+// opg 1 med en for løkke
+console.log("opg 1 med for løkke")
+for(let i=10;i>=0;i--){
+    console.log(i);
+} 
