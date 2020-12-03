54c64f9682716d3ae357aa1f8ad20e638f33b916
lovetildigAnders <asylvesterp@gmail.com>
2020-12-03 10:38:48 +0100

    dont work but makes balls

--- more-moving/sketch.js
+++ more-moving/sketch.js
@@ -1,42 +1,39 @@
-let x= new array (5)
-let y= new array (5)
-for(let i=0;i<x,length;i++)
-for(let b=0;b<y,length;b++)
-let goleft =false; 
-let godown =false; 
+let ballcount =5
+let ds= new Array(ballcount);
+let ys = new Array(ballcount);
+let xs = new Array(ballcount);
+let xSpeed = 5
+let ySpeed = 3
+
 function setup() {
-  createCanvas(400, 400);
+createCanvas(500,500);
+
+for (let i = 0; i < ballcount; i++) {
+ds[i]= random(5,50)
+  }
+
+for (let i = 0; i < ballcount; i++) {
+  xs[i]= random(0,innerWidth);
+  ys[i]= random(0,innerHeight)
+  }
+
 }
-function draw() {
-  background(200,200,0);
-  circle(x, y, 30);
-if(goleft == false){
-  x=x+1,5
+function draw(){
+  background(200);
+for (let i = 0; i < ballcount; i++) {
+  circle(xs[i],ys[i],ds[i]);
+  xs[i]++
+  ys[i]++
 }
-if(x>400) 
-{
-  goleft =true;
-}
-if(goleft == true){
-  x=x-2
-} 
-if(x<0)
-{
-  goleft = false; 
+if (xs[i] > width - ds[i] / 2||xs[i] < 0 + ds[i] / 2) {
+  xSpeed = -xSpeed;
 }
 
-if(godown == false){
-  y=y+3
+if (ys[i] > height - ds[i] / 2 || ys[i] < 0 + ds[i] / 2) {
+  ySpeed = -ySpeed;
 }
-if(y>400) 
-{
-  godown =true;
+
+xs[i] = xs[i] + xSpeed;
+ys[i] = ys[i] + ySpeed;
 }
-if(godown == true){
-  y=y-0.5
-} 
-if(y<0)
-{
-  godown = false; 
-}
-}
+
--- move/sketch.js
+++ move/sketch.js
@@ -11,30 +11,26 @@ function draw() {
 if(goleft == false){
   x=x+1,5
 }
-if(x>400) 
-{
+if(x>400) {
   goleft =true;
 }
 if(goleft == true){
   x=x-2
 } 
-if(x<0)
-{
+if(x<0){
   goleft = false; 
 }
 
 if(godown == false){
   y=y+3
 }
-if(y>400) 
-{
+if(y>400){
   godown =true;
 }
 if(godown == true){
   y=y-0.5
 } 
-if(y<0)
-{
+if(y<0){
   godown = false; 
 }
 }
